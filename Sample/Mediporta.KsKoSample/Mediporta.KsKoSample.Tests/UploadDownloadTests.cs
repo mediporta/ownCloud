114f3fb23ec243b2e4daac14d01d7487438496a3
Mateusz Szynka <mateusz.szynka@mediporta.pl>
2018-05-16 14:23:29 +0200

    Generowanie plików testowych

--- Sample/Mediporta.KsKoSample/Mediporta.KsKoSample.Tests/UploadDownloadTests.cs
+++ Sample/Mediporta.KsKoSample/Mediporta.KsKoSample.Tests/UploadDownloadTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 using Mediporta.KsKoSample.Extensions;
 using Mediporta.KsKoSample.Tests.Factories;
 using NUnit.Framework;
@@ -29,29 +28,45 @@ namespace Mediporta.KsKoSample.Tests
         }
 
         [Test]
-        public void It_can_upload_and_download_correct_file()
+        [TestCase(0.1)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void It_can_upload_and_download_correct_file(double size)
         {
             var client = testClientFactory.GetTestClient();
-
-            var uploadResult = client.Upload(remotePath, GetSampleFile(), "application/zip");
-
+            var fileStream = GetSampleFile(size);
+            var uploadResult = client.Upload(remotePath, fileStream, "application/zip");
             var existResult = client.Exists(remotePath);
-
             var ocHash = testClientFactory.GetCustomWebdavClient().GetHash(remotePath);
-
-            var fileHash = CryptoExtensions.ComputeSha1Hash(GetSampleFile());
-            
+            fileStream.Seek(0, SeekOrigin.Begin);
+            var fileHash = CryptoExtensions.ComputeSha1Hash(fileStream);
             var downloadStream = client.Download(remotePath);
 
             Assert.That(uploadResult, Is.True);
             Assert.That(existResult, Is.True);
             Assert.That(fileHash, Is.EqualTo(ocHash));
-            Assert.That(StreamExtensions.StreamsAreIdentical(GetSampleFile(), downloadStream));
+            fileStream.Seek(0, SeekOrigin.Begin);
+            Assert.That(StreamExtensions.StreamsAreIdentical(fileStream, downloadStream));
         }
-        
-        private Stream GetSampleFile()
+
+        private Stream GetSampleFile(double size)
         {
-            return Assembly.GetExecutingAssembly().GetManifestResourceStream("Mediporta.KsKoSample.Tests.sampleInput.zip");
+            // Note: block size must be a factor of 1MB to avoid rounding errors :)
+            const int blockSize = 1024 * 8;
+            const int blocksPerMb = (1024 * 1024) / blockSize;
+            var data = new byte[blockSize];
+            var rng = new Random();
+            using (var stream = File.OpenWrite("samplefile"))
+            {
+                // There 
+                for (int i = 0; i < size * blocksPerMb; i++)
+                {
+                    rng.NextBytes(data);
+                    stream.Write(data, 0, data.Length);
+                }
+
+                return stream;
+            }
         }
     }
 }
