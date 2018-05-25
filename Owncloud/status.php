c073736589c412007205358f41058eea9f10e030
Mateusz Szynka <mateusz.szynka@mediporta.pl>
2018-05-25 13:47:02 +0200

    Status fix

--- Owncloud/status.php
+++ Owncloud/status.php
@@ -34,7 +34,8 @@
 
 	require_once __DIR__ . '/lib/base.php';
 
-	global $instrumentationKey = \OC::$server->getConfig()->getSystemValue('azure.instrumentationkey', 'NULL');
+	global $instrumentationKey;
+	$instrumentationKey = \OC::$server->getConfig()->getSystemValue('azure.instrumentationkey', 'NULL');
 
 	if($instrumentationKey != 'NULL'){
 		require_once 'vendor/autoload.php';
